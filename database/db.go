160f56d2c6c1e076dceab70f84bcf878b32bcdb7
vinicius bosso <104734169+vinyhi@users.noreply.github.com>
2024-09-27 15:27:09 +0200

    optimize

--- database/db.go
+++ database/db.go
@@ -16,27 +16,28 @@ var db *gorm.DB
 
 func init() {
 	// Load .env file
-	err := godotenv.Load()
-	if err != nil {
+	if err := godotenv.Load(); err != nil {
 		log.Println("Error loading .env file, continuing with system environment variables")
 	}
 
+	initializeDatabase()
+}
+
+func initializeDatabase() {
 	// Define PostgreSQL connection string
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
 		os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
 
 	// Connect to PostgreSQL using GORM
-	var errConnect error
-	db, errConnect = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if errConnect != nil {
-		log.Fatalf("Failed to connect to database: %v", errConnect)
+	var err error
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
 	}
 
 	// Migrate the schema and check for migration error
-	errMigration := db.AutoMigrate(&models.Project{}, &models.Task{})
-	if errMigration != nil {
-		log.Printf("Failed to auto-migrate database schema: %v", errMigration)
-		// Optionally, handle migration errors differently e.g., by not halting the application.
+	if err := db.AutoMigrate(&models.Project{}, &models.Task{}); err != nil {
+		log.Printf("Failed to auto-migrate database schema: %v", err)
 	}
 }
 
@@ -45,6 +46,7 @@ func GetDB() *gorm.DB {
 	if db == nil {
 		log.Println("Database connection has not been established.")
 		// Initialize or reconnect logic can go here
+		initializeDatabase() // Consider implementing a more robust check or reconnection logic
 	}
 	return db
 }
@@ -53,7 +55,6 @@ func main() {
 	// Example usage
 	db := GetDB()
 
-	// Confirm the db is not nil before proceeding to prevent panic
 	if db == nil {
 		log.Println("Failed to get database connection")
 		return
